4f687612998ae16ba383f1444355e2ff6cc7d04a
Henry Yanqui <hyanquiv@unsa.edu.pe>
2025-07-31 11:51:04 -0500

    Correccion

--- aligner.py
+++ aligner.py
@@ -2,8 +2,8 @@ import subprocess
 from shutil import which
 
 def run_alignment(input_file, output_file):
-    if not which("muscle"):
-        raise EnvironmentError("MUSCLE no está en el PATH. Instálalo o colócalo en ./muscle/")
-
-    cmd = ["muscle", "-in", input_file, "-out", output_file]
+    if not which("aligner"):
+        raise EnvironmentError("aligner no está en el PATH. Instálalo o colócalo en ./aligner/")
+    algn_path = "./aligner.exe"
+    cmd = [algn_path, "-align", input_file, "-output", output_file]
     subprocess.run(cmd, check=True)
